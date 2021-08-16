2483aceae6c665ed23a738b56fce28bd0167a3b6
Noir <griffliway@gmail.com>
2021-08-16 19:30:16 +0300

    New app icon and name

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firstapp/pages/Home.dart';
+import 'package:ToDo/pages/Home.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
